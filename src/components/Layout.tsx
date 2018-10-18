23128bbb186647c799381d14f9b4932805bdb927
shiyangyang <shiyangyang@flowpp.com>
2018-10-18 19:41:16 +0800

    v0.2.7

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,43 +3,52 @@ import Screen from '../containers/Screen'
 import Hierarchy from '../containers/Hierarchy'
 import Detail from '../containers/Detail'
 
-export default ({doc, HEIGHT = 750}) => !doc ? <h2>Loading...</h2> : <div style={{
-    position: 'relative',
-    width: '100%',
-    height: HEIGHT,
-    border: '1px solid #ccc'
-}}>
-    <div style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        width: '50%',
-        height: HEIGHT,
-        background: '#272822'
-    }}><Screen HEIGHT={HEIGHT}/></div>
-    <div style={{
-        position: 'absolute',
-        right: 0,
-        top: 0,
-        width: '50%',
-        height: HEIGHT
-    }}>
-        <div style={{
-            position: 'absolute',
-            left: 0,
-            top: 0,
+export interface LayoutProps {
+    doc
+    HEIGHT: number
+} 
+export default class extends Component<LayoutProps> {
+    render () {
+        const { doc, HEIGHT } = this.props
+        return !doc ? <h2>Loading...</h2> : <div style={{
+            position: 'relative',
             width: '100%',
-            height: HEIGHT / 2,
-            overflow: 'auto'
-        }}><Hierarchy /></div>
-        <div style={{
-            position: 'absolute',
-            left: 0,
-            bottom: 0,
-            width: '100%',
-            height: HEIGHT / 2,
-            borderTop: '1px solid #ccc',
-            overflow: 'auto'
-        }}><Detail /></div>
-    </div>
-</div>
+            height: HEIGHT,
+            border: '1px solid #ccc'
+        }}>
+            <div style={{
+                position: 'absolute',
+                left: 0,
+                top: 0,
+                width: '50%',
+                height: HEIGHT,
+                background: '#272822'
+            }}><Screen HEIGHT={HEIGHT}/></div>
+            <div style={{
+                position: 'absolute',
+                right: 0,
+                top: 0,
+                width: '50%',
+                height: HEIGHT
+            }}>
+                <div style={{
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    width: '100%',
+                    height: HEIGHT / 2,
+                    overflow: 'auto'
+                }}><Hierarchy /></div>
+                <div style={{
+                    position: 'absolute',
+                    left: 0,
+                    bottom: 0,
+                    width: '100%',
+                    height: HEIGHT / 2,
+                    borderTop: '1px solid #ccc',
+                    overflow: 'auto'
+                }}><Detail /></div>
+            </div>
+        </div>
+    }
+}
