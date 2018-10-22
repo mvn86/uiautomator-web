5c1492ddf3446b371482389d3b59e92f712e40f3
shiyangyang <shiyangyang@flowpp.com>
2018-10-22 11:47:58 +0800

    v0.2.8

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { Config } from './interfaces'
 import { dispatch } from './store'
-import { h, render, VNode } from 'preact'
+import { h, render, VNode, Component } from 'preact'
 import Layout from './containers/Layout'
 
 export const renderContainer = (cfg: Config) => {
@@ -9,30 +9,44 @@ export const renderContainer = (cfg: Config) => {
     render(<Container {...props}  HEIGHT={height}/>, el)
 }
 
-export const Container = (cfg: Config) => {
-    const { HEIGHT, loadXML, screenShot, onload, onClick, onInput, onerror = e => alert('xml 加载失败！\n' + e.toString()) } = cfg
-    const img = new Image()
-    dispatch(state => ({needReload: true}))
-    img.addEventListener('load', function (e) {
-        const { width, height } = img
-        onload && onload(img)
-        loadXML().then(doc => {
-            const [x, y, w, h] = doc.querySelector('[bounds]').getAttribute('bounds').match(/\d+/g)
-            dispatch(state => ({
-                ...state,
-                width: Number(w), height: Number(w) * height / width,
-                doc,
-                focus: null,
-                onClick,
-                onInput,
-                expends: new Set<Element>()
-            }))
-        }).catch(onerror)
-    })
-    screenShot().then((src) => {
-        img.src = src;
-        dispatch(state => ({...state, src}))
-    })
+export class Container extends Component<Config> {
+
+    init = (props: Config) => {
+        const { loadXML, screenShot, onload, onClick, onInput, onerror = e => alert('xml 加载失败！\n' + e.toString()) } = props
+        const img = new Image()
+        dispatch(state => ({...state, needReload: true}))
+        img.addEventListener('load', function (e) {
+            const { width, height } = img
+            onload && onload(img)
+            loadXML().then(doc => {
+                const [x, y, w, h] = doc.querySelector('[bounds]').getAttribute('bounds').match(/\d+/g)
+                dispatch(state => ({
+                    ...state,
+                    width: Number(w), height: Number(w) * height / width,
+                    doc,
+                    src: img.src,
+                    focus: null,
+                    onClick,
+                    onInput,
+                    expends: new Set<Element>()
+                }))
+            }).catch(onerror)
+        })
+        screenShot().then((src) => {
+            img.src = src;
+        })
+    }
+
+    componentDidMount () {
+        this.init(this.props)
+    }
     
-    return <Layout HEIGHT={HEIGHT}/>
+    componentWillReceiveProps (nextProps) {
+        this.init(nextProps)
+    }
+    
+
+    render () {
+        return <Layout HEIGHT={this.props.HEIGHT}/>
+    }
 }
